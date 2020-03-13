bbeda0fab52b5128f1c3b2b33d70291408736c42
Rabindra80s <rabindra.mdr@outlook.com>
2020-03-13 14:45:06 +0200

    few unit tests added and app deployed in firebase

--- client/src/containers/BurgerBuilder/BurgerBuilder.js
+++ client/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,7 +8,7 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 import * as actions from '../../store/actions/index';
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import axios from "../../axios-orders";
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
 
   state = {
     purchasing: false
